3a13b8400a8caca384616838b00911ce2d5238d5
Zals <rahmataprizal403@gmail.com>
2024-07-09 21:02:15 +0700

    recommendations.js

--- api/recommendations.js
+++ api/recommendations.js
@@ -5,6 +5,11 @@ module.exports = async (req, res) => {
   if (!videoId) {
     return res.status(400).send('ID video tidak ditemukan');
   }
-  const result = await ytsearch({ videoId });
-  res.json(result.related);
+  try {
+    const result = await ytsearch({ videoId });
+    res.json(result.related);
+  } catch (error) {
+    console.error('Error during fetching recommendations:', error);
+    res.status(500).send('Error during fetching recommendations');
+  }
 };
