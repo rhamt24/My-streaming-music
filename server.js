757badfb8fe8e6b3c25053252588362c91d86f39
Zals <rahmataprizal403@gmail.com>
2024-07-09 20:58:50 +0700

    server.js

--- server.js
+++ server.js
@@ -1,23 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
-
-app.get('/api/search', async (req, res) => {
-  console.log('Search query received:', req.query.q);
-  // kode search...
-});
-
-app.get('/api/stream', (req, res) => {
-  console.log('Stream request received for video ID:', req.query.id);
-  // kode stream...
-});
-
-app.get('/api/recommendations', async (req, res) => {
-  console.log('Recommendations request received for video ID:', req.query.id);
-  // kode recommendations...
-});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`);
