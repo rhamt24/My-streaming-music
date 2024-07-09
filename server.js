13b5487f25ed338dbeceba0fdcaa2b572a45bf5b
Zals <rahmataprizal403@gmail.com>
2024-07-09 20:48:31 +0700

    server.js

--- server.js
+++ server.js
@@ -4,6 +4,21 @@ const port = 3000;
 
 app.use(express.static('public'));
 
+app.get('/api/search', async (req, res) => {
+  console.log('Search query received:', req.query.q);
+  // kode search...
+});
+
+app.get('/api/stream', (req, res) => {
+  console.log('Stream request received for video ID:', req.query.id);
+  // kode stream...
+});
+
+app.get('/api/recommendations', async (req, res) => {
+  console.log('Recommendations request received for video ID:', req.query.id);
+  // kode recommendations...
+});
+
 app.listen(port, () => {
   console.log(`Server berjalan di http://localhost:${port}`);
 });
